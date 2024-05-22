5adf8189119abee8efd6aa544041f473fe127ed0
enea-scaccabarozzi <me@eneascaccabarozzi.xyz>
2024-05-22 09:13:28 +0200

    feat: improve library interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
 export { Binance } from './binance';
 export { Bybit } from './bybit';
+
+export {
+  Balance,
+  CurrencyBalance,
+  GetBalanceOptions,
+  SetLeverageOptions,
+} from './types/shared/account';
+export { TradekitError, TradekitResult } from './types/shared/errors';
+export { Tradekit, TradekitAuth, TradekitOptions } from './types/shared/index';
+export {
+  Order,
+  OpenPositionOptions,
+  ClosePositionOptions,
+} from './types/shared/orders';
+export { ProxyAuth, ProxyOptions, ProxyProtocol } from './types/shared/proxy';
+export {
+  Ticker,
+  GetTikerOptions,
+  GetTikersOptions,
+  SubscribeToTikerOptions,
+  SubscribeToTikersOptions,
+} from './types/shared/tickers';
+export { StreamClient } from './types/shared/websocket';
